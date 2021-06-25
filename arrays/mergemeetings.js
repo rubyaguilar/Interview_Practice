2cfd0dde5fa3bf972fe37a6cbe0e204cc8ce0281
Ruben Aguilar <ruben.aguilar@mutualofomaha.com>
2021-06-25 08:40:22 -0500

    done with mergemeetings

--- arrays/mergemeetings.js
+++ arrays/mergemeetings.js
@@ -1,6 +1,43 @@
 
-function mergemeetings(meetings){
 
+function bubbleSortTimes(meetings){
+
+    for(let i = 0; i < meetings.length-1; i++){
+        if(meetings[i].startTime > meetings[i+1].startTime){
+            let copy = meetings[i]
+            meetings[i] = meetings[i+1]
+            meetings[i+1] = copy
+        }
+    }
+
+    return meetings
 }
 
-const meetings =
+function mergeMeetings(meetings){
+
+    meetings = bubbleSortTimes(meetings)
+
+    for(let i = 0; i < meetings.length-1; i++){
+        if (meetings[i].startTime <= meetings[i+1].startTime &&
+            meetings[i].endTime >= meetings[i+1].endTime ) {
+            meetings.splice(i+1, 1)
+            i--
+        }
+        else if( (meetings[i].endTime >= meetings[i+1].startTime) ){
+            meetings[i].endTime = meetings[i+1].endTime
+            meetings.splice(i+1, 1)
+            i--
+        }
+    }
+
+    return meetings;
+}
+
+const meetings = [
+    { startTime: 1, endTime: 10 },
+    { startTime: 2, endTime: 5 },
+    { startTime: 6, endTime: 8 },
+    { startTime: 9, endTime: 10 },
+    { startTime: 10, endTime: 12 }]
+
+console.log(mergeMeetings(meetings))
